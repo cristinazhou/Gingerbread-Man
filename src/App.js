73b9007a41a7915e621cad7ecb432ff1ee71705c
cristinazhou <wendybang@126.com>
2018-12-16 19:28:58 +0800

    initial page~

--- src/App.js
+++ src/App.js
@@ -1,29 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
-import Background from './background.png';
+import Background from './crown.png';
+import { Icon } from 'antd';
+
 var sectionStyle = {
     width: "100%",
     height: "100%",
-// makesure here is String确保这里是一个字符串，以下是es6写法
     backgroundImage: `url(${Background})`,
-    paddingTop:"40px",
-    paddingRight:"150px",
-    paddingLeft:"100px",
-    backgroundSize:"100% 100%",
+    backgroundSize:"72% 100%",
     backgroundRepeat:"no-repeat",
+    backgroundPosition:"50% 30%",
+
+
 };
 class App extends Component {
   render() {
     return (
       <div className="App" style={sectionStyle}>
-        <header className="App-header">
-<div >
-          <p>
-           The complex made simple.We help you take a note efficiently.
-          </p>
-    <p> Gingerbread Man</p>
-</div>
-        </header>
+          <div className="header">
+          <div className="icons-list" >
+          <Icon type="align-left" spin="true"  twoToneColor="#ffffff"  style={{ fontSize: '32px' }} />
+          </div>
+      <div className="logo"> GingerNote</div>
+              <div className="icons-list2" >
+              <Icon type="user"  style={{ fontSize: '32px' }} />
+                  <Icon type="search"  style={{ fontSize: '32px' }}/>
+              </div>
+          </div>
+
+
+          <header className="App-header">
+          <div className="prankcallersmasqu"> The complex made simple.</div>
+      <div className="button"> Get Started</div>
+          </header>
       </div>
     );
   }
